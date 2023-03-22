d16d2ae719698438ada2f264528b4262538ae759
Sayad Aazami <sayadaazami@gmail.com>
2023-03-22 18:18:29 +0430

    init

---
+++ apps/aiowl/pages/index.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components';
+
+const StyledPage = styled.div`
+  .page {
+  }
+`;
+
+export function Index() {
+  return (
+    <StyledPage>
+      Home
+    </StyledPage>
+  );
+}
+
+export default Index;
